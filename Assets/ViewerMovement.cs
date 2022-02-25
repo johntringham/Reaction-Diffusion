8ad62c71b009d527f96b58ae2faf00765c04c867
John Tringham <john@tringham.co.uk>
2022-02-25 17:07:56 +0000

    good stuff

---
+++ Assets/ViewerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewerMovement : MonoBehaviour
+{
+    public Transform DesiredTransform;
+
+    public float PositionLerp = 10.0f;
+    public float RotationSlerp = 10f;
+    
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = DesiredTransform.position;
+        //this.transform.position = Vector3.Lerp(this.transform.position, DesiredTransform.position, PositionLerp * Time.deltaTime);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, DesiredTransform.rotation, RotationSlerp * Time.deltaTime);
+    }
+}
